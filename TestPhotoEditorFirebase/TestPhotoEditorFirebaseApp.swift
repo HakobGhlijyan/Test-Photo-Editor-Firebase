9a1a291145c6d15437a9338d22976f6ba7340197
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-08-01 13:54:12 +0400

    Initial Commit

---
+++ TestPhotoEditorFirebase/TestPhotoEditorFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestPhotoEditorFirebaseApp.swift
+//  TestPhotoEditorFirebase
+//
+//  Created by Hakob Ghlijyan on 01.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestPhotoEditorFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
